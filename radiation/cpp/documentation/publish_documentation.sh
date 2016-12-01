ff5d3f14deec35a9f53f86429bf3321b87b8c94b
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2016-11-30 22:27:51 -0800

    Automated documentation build for changeset d9e3f0deb5edc29e8ca3975f0bd8b04dc5c86df0.

---
+++ radiation/cpp/documentation/publish_documentation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# Quit on errors.
+set -o errexit -o nounset
+
+# Settings.
+DOCUMENTATION_PATH=radiation/cpp/documentation
+CHANGESET=$(git rev-parse --verify HEAD)
+
+# Make documentation.
+doxygen Doxyfile
+
+# Commit documentation in master.
+cd ../../..
+git add ${DOCUMENTATION_PATH}
+git commit -m "adding documentation"
+
+# Check out gh-pages branch and merge documentation from master commit.
+git checkout gh-pages
+rm -r ${DOCUMENTATION_PATH}
+git checkout master ${DOCUMENTATION_PATH}
+
+# Add the merged changes and push.
+git commit -a -m "Automated documentation build for changeset ${CHANGESET}."
+git push -u origin gh-pages
+
+# Checkout master again and blow away generated docs.
+git checkout master
+git rm ${DOCUMENTATION_PATH}/html/ ${DOCUMENTATION_PATH}/latex/
+
+echo "-- Successfully updated documentation!"
