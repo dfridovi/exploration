1c5b7d30888e504a50c311f0f330235f0c2d6746
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2016-12-12 17:47:38 -0800

    Automated documentation build for changeset 194a3fa573997e557538ea8c01d9e312cb4a037c.

--- radiation/cpp/documentation/html/search/classes_1.js
+++ radiation/cpp/documentation/html/search/classes_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['explorer2d',['Explorer2D',['../classradiation_1_1_explorer2_d.html',1,'radiation']]],
   ['explorerlp',['ExplorerLP',['../classradiation_1_1_explorer_l_p.html',1,'radiation']]],
-  ['explorerrw',['ExplorerRW',['../classradiation_1_1_explorer_r_w.html',1,'radiation']]]
+  ['explorerrw',['ExplorerRW',['../classradiation_1_1_explorer_r_w.html',1,'radiation']]],
+  ['explorersocp',['ExplorerSOCP',['../classradiation_1_1_explorer_s_o_c_p.html',1,'radiation']]]
 ];
